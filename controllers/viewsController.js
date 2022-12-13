99e3f2d82f03822d5c502a64acd05ce6d82c5eab
Rami Alkawadri <ramialkawadri@hotmail.com>
2022-12-13 12:44:37 +0100

    Adding templates

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,11 +1,23 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res) => {
+  const tours = await Tour.find();
+
   res.status(200).render('overview', {
     title: 'All tours',
+    tours,
   });
-};
+});
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker tour',
+exports.getTour = catchAsync(async (req, res) => {
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
   });
-};
+
+  res.status(200).render('tour', {
+    title: tour.name,
+    tour,
+  });
+});
